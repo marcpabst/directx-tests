9c701c921b2fcbb0b8f2287ed28824228ca73b3c
m. pabst <johndoe@example.com>
2024-05-29 23:10:35 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -847,9 +847,8 @@ mod d3d12_hello_triangle {
         }
 
         // add the timestamp to frame_rate_calc
-        resources
-            .frame_rate_calc
-            .count_cycle(last_vblank as f64 / qpc_frequency as f64 * 1_000_000.0);
+        let last_vblank_ms = last_vblank as f64 / qpc_frequency as f64 * 1_000.0;
+        resources.frame_rate_calc.count_cycle(last_vblank_ms);
 
         // get current estimated fps
         let fps = resources.frame_rate_calc.get_current_frequency();
