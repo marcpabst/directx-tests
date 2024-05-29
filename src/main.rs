c92494ca484c1fd2048acbf075fdee35d053c457
m. pabst <johndoe@example.com>
2024-05-29 19:35:43 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -443,7 +443,7 @@ mod d3d12_hello_triangle {
                 wait_for_previous_frame(resources);
 
                 // stall htread for by a random amount of time between 0 and 10ms
-                let stall_time = rand::random::<u64>() % 20;
+                let stall_time = rand::random::<u64>() % 16;
                 std::thread::sleep(std::time::Duration::from_millis(stall_time));
             }
         }
