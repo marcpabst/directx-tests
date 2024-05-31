01d77eda481ae0ec39912a4ab4a52e32daee5c70
m. pabst <johndoe@example.com>
2024-06-01 00:00:38 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -1132,7 +1132,7 @@ fn main() -> Result<()> {
                 println!("Current estimated FPS: {}", fps);
 
                 // we can sleep here to avoid busy waiting
-                std::thread::sleep(std::time::Duration::from_millis(10));
+                std::thread::sleep(std::time::Duration::from_millis(1));
             }
 
             was_in_vblank = is_in_vblank;
